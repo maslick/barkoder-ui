4c17f4bf38d27aa21e34b27b3c2db6168f1d5e95
Pavel Maslov <pavel.maslov@ineor.si>
2019-03-14 20:26:08 +0100

    simplified the configure.sh script

--- configure.sh
+++ configure.sh
@@ -1,14 +1,8 @@
 #!/bin/bash -eu
-if [[ -z "${KC_URL:-}" ]]; then KC_URL="undefined"; else KC_URL=$KC_URL; fi
-if [[ -z "${REALM:-}" ]]; then REALM="undefined"; else REALM=$REALM; fi
-if [[ -z "${CLIENT_ID:-}" ]]; then CLIENT_ID="undefined"; else CLIENT_ID=$CLIENT_ID; fi
-if [[ -z "${KC_ROLE:-}" ]]; then KC_ROLE="undefined"; else KC_ROLE=$KC_ROLE; fi
-if [[ -z "${BACKEND_URL:-}" ]]; then BACKEND_URL="undefined"; else BACKEND_URL=$BACKEND_URL; fi
-
 cat <<EOF
-window.kcUrl="$KC_URL";
-window.realm="$REALM";
-window.clientId="$CLIENT_ID";
-window.kcRole="$KC_ROLE";
-window.baseUrl="$BACKEND_URL";
+window.kcUrl="${KC_URL:-}";
+window.realm="${REALM:-}";
+window.clientId="${CLIENT_ID:-}";
+window.kcRole="${KC_ROLE:-}";
+window.baseUrl="${BACKEND_URL:-}";
 EOF
